2f9cf3ef73fbb2845a002592495ab3026a740dc0
Matt Teixeira <matt-teixeira@vm-debian.c.oceanic-citadel-185620.internal>
2024-01-15 22:00:02 +0000

    Filter system data based on user's report criteria

--- jobs/helium_level.js
+++ jobs/helium_level.js
@@ -1,3 +1,54 @@
-const filter_on_models_list = async () => {
-    
-}
+const { build_email_text } = require("../tools");
+
+// 1) Filter on user’s operator and custom_threshold criteria
+// 2) Get filtered data into HTML
+// 3) Send email report
+const helium_level_reports = async (user_reports) => {
+  //console.log(user_reports);
+  const {
+    author,
+    field_name,
+    operator,
+    custom_threshold,
+    threshold_data_type,
+    cc_list
+  } = user_reports;
+
+  const report_meta_data = {
+    author,
+    field_name,
+    operator,
+    custom_threshold,
+    threshold_data_type,
+    cc_list
+  };
+
+  console.log(report_meta_data);
+
+  const reportable_reports = [];
+  for (let report of user_reports.matched_model_data) {
+    if (report.helium_value === null) continue;
+    switch (user_reports.operator) {
+      case "less_than":
+        if (report.helium_value < user_reports.custom_threshold)
+          reportable_reports.push(report);
+        break;
+      case "greater_than":
+        if (report.helium_value > user_reports.custom_threshold)
+          reportable_reports.push(report);
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  console.log("\nReport This Data!");
+  console.log(reportable_reports);
+
+  console.log("\n Build Email");
+
+  build_email_text(report_meta_data, reportable_reports);
+};
+
+module.exports = helium_level_reports;
---
+++ jobs/index.js
@@ -0,0 +1,3 @@
+const helium_level_reports = require("./helium_level");
+
+module.exports = { helium_level_reports };
